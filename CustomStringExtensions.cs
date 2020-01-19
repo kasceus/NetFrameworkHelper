1d230bf072c1de071fe744bd1d416c4a621027a9
kasceus <chaz92487@gmail.com>
2020-01-18 23:52:03 -1000

    Add project files.

---
+++ CustomStringExtensions.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace NetFrameworkHelper
+{
+	/// <summary>
+	/// This class contains custom extensions for use with strings
+	/// </summary>
+	public static class CustomStringExtensions
+	{
+		/// <summary>
+		/// Returns a string literal from a specified number of characters from the right of a string
+		/// </summary>
+		/// <param name="str">sting this method is called against</param>
+		/// <param name="start">Number of characters to return from the right side of a string</param>
+		/// <returns>String</returns>
+		public static string Right(this string str, int start)
+		{
+			if (String.IsNullOrEmpty(str))
+			{
+				throw new ArgumentNullException(nameof(str), "String cannot be null or empty");
+			}
+			if (start > str.Length)
+			{
+				throw new ArgumentOutOfRangeException(nameof(str), "Number of characters greater than the string length");
+			}
+			return str.Substring(str.Length - start, start);
+		}
+		/// <summary>
+		/// Returns a string literal from a specified number of characters from the start of a string
+		/// </summary>
+		/// <param name="str">sting this method is called against</param>
+		/// <param name="nCount">Number of characters to return from the left</param>
+		/// <returns>String</returns>
+		public static string Left(this string str, int nCount)
+		{
+			if (String.IsNullOrEmpty(str))
+			{
+				throw new ArgumentNullException(nameof(str), "String cannot be null or empty");
+			}
+			if (nCount > str.Length)
+			{
+				throw new ArgumentOutOfRangeException(nameof(str), "Number of characters greater than the string length");
+			}
+			return str.Substring(0, nCount);
+		}
+		/// <summary>
+		/// Returns a string from a specified start position
+		/// </summary>
+		/// <param name="str">sting this method is called against</param>
+		/// <param name="start">Character number to start with</param>
+		/// <returns>String</returns>
+		public static string Mid(this string str, int start)
+		{
+			if (String.IsNullOrEmpty(str))
+			{
+				throw new ArgumentNullException(nameof(str), "String cannot be null or empty");
+			}
+			if (start > str.Length)
+			{
+				throw new ArgumentOutOfRangeException(nameof(str), "Number of characters greater than the string length");
+			}
+
+			return str.Substring(start);
+		}
+		/// <summary>
+		/// Returns a string from a specified start position
+		/// </summary>
+		/// <param name="str">sting this method is called against</param>
+		/// <param name="start">Character number to start with</param>
+		/// <param name="stop">Character number to stop with</param>
+		/// <returns>String</returns>
+		public static string Mid(this string str, int start, int stop)
+		{
+			if (String.IsNullOrEmpty(str))
+			{
+				throw new ArgumentNullException(nameof(str), "String cannot be null or empty");
+			}
+			if (start > str.Length)
+			{
+				throw new ArgumentOutOfRangeException(nameof(str), "Number of characters greater than the string length");
+			}
+			//Check if start is less than 0
+			start = (start < 0) ? 0 : start;
+			//check if the requested stop position is longer than the returnes tring would be minus the start position
+			//if it is, then set stop to the string length minus the start position
+			stop = (stop > (str.Length - start)) ? str.Length - start : stop;
+			return str.Substring(start, stop);
+		}
+	}
+}
---
+++ FileHelper.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Web.Mvc;
+using System.Web;
+
+namespace Net_Framework_Helper
+{
+	/// <summary>
+	/// The FileHelper is used for common file operations
+	/// </summary>
+	public static class FileHelper
+	{
+		/// <summary>
+		/// This extension method will retun a FileContentResult from a path to a file.  
+		/// <para>This result is return ready- includes mime-type for the specified file</para>
+		/// </summary>
+		/// <param name="pathToFile">Full path to a file</param>
+		/// <returns>FileCointentResult</returns>
+		public static FileContentResult FileContentResultFromString(this string pathToFile)
+		{
+			if (File.Exists(pathToFile))
+			{
+				return new FileContentResult(pathToFile.FileBytesFromPath(), pathToFile.GetMimeFromFilePath());
+			}
+			else
+			{
+				throw new FileNotFoundException(nameof(pathToFile), "The file you requested was not found.");
+			}
+		}
+		/// <summary>
+		/// Get a mime type for a file path
+		/// </summary>
+		/// <param name="pathToFile"></param>
+		/// <returns></returns>
+		public static string GetMimeFromFilePath(this string pathToFile)
+		{
+			try
+			{
+				FileAttributes attr = File.GetAttributes(pathToFile);
+				if (attr.HasFlag(FileAttributes.Directory))
+				{
+					throw new Exception("The specified path is a directory. Please supply the file name in addition to the file.");
+				}
+			}
+			catch
+			{
+				throw;
+			}
+			if (File.Exists(pathToFile))
+			{
+				var fileName = Path.GetFileName(pathToFile);
+				return fileName.GetMimeFromFileName();
+			}
+			else
+			{
+				throw new FileNotFoundException("File not found.");
+			}
+		}
+		/// <summary>
+		/// Get a mime type for a file name
+		/// </summary>
+		/// <param name="fileName"></param>
+		/// <returns></returns>
+		public static string GetMimeFromFileName(this string fileName)
+		{
+			try
+			{
+				FileAttributes attr = File.GetAttributes(fileName);
+				if (attr.HasFlag(FileAttributes.Directory))
+				{
+					throw new Exception("The specified path is a directory. Please supply the file name.");
+				}
+			}
+			catch
+			{
+				throw;
+			}
+			return MimeMapping.GetMimeMapping(fileName);
+		}
+
+		/// <summary>
+		/// Returns the byte array for a specified file path
+		/// </summary>
+		/// <param name="pathToFile">File Path string</param>
+		/// <returns>byte[]</returns>
+		public static byte[] FileBytesFromPath(this string pathToFile)
+		{
+			if (File.Exists(pathToFile))
+			{
+				return File.ReadAllBytes(pathToFile);
+			}
+			else
+			{
+				throw new FileNotFoundException(nameof(pathToFile), "The file you requested was not found.");
+			}
+		}
+		/// <summary>
+		/// Useful for deleting a file given the full path to the file
+		/// </summary>
+		/// <param name="pathToFile">path to the file</param>
+		public static void DeleteFile(this string pathToFile)
+		{
+			try
+			{
+				FileAttributes attr = File.GetAttributes(pathToFile);
+				if (attr.HasFlag(FileAttributes.Directory))
+				{
+					throw new Exception("The specified path is a directory. Please supply the file name in addition to the file.");
+				}
+			}
+			catch
+			{
+				throw;
+			}
+			if (File.Exists(pathToFile))
+			{
+				try
+				{
+					File.Delete(pathToFile);
+				}
+				catch
+				{ //catch any exceptions and rethrow them
+					throw;
+				}
+			}
+			else
+			{
+				throw new FileNotFoundException("The file was not found");
+			}
+		}
+
+	}
+}
